5bd187267fa5d3491b063b89558169d992688427
HugoPadilla1 <hpadilla@ggc.edu>
2024-01-12 11:21:31 -0500

    initial

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args){
+        var temperature = 25;
+        System.out.println("The temperature is " + temperature);
+
+        var firstName = "Hugo";
+        var lastName = "Padilla";
+        System.out.println("My name is " + firstName + " " + lastName);
+    }
+}
